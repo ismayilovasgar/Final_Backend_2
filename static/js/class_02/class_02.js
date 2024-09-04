1f41a3b2489c6392d7f410f0822ef9d0e00a0662
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-09-04 23:02:26 +0400

    save new version

--- static/js/class_02/class_02.js
+++ static/js/class_02/class_02.js
@@ -92,7 +92,7 @@ async function fetchFilteredData(text, wrap) {
   );
   const data = await response.json();
   console.log(data);
-
+  fillDemo(data, wrap);
   //* data.map((item) => {
   //   console.log(item.fullname);
   //*   item.courses.map((course) => {
@@ -102,8 +102,127 @@ async function fetchFilteredData(text, wrap) {
 }
 
 async function fillDemo(data, wrap) {
-  
+  wrap.innerHTML = "";
+  data.map((item) => {
+    wrap.innerHTML += `
+    <div class="trainerItem">
+            <div class="profile">
+              <img src="${item.trainer_image}" alt="">
+            </div>
+            <div class="trainerName">${item.fullname}</div>
+            <div class="trainerPosition"> ${item.profession} </div>
+    </div>
 
+    <div class="modal" id="">
+                    <div class="modalContent">
+                        <div class="popupContainer">
+
+                            <div class="trainerTop">
+                                <div class="trainerAvatar">
+                                    <img src="${item.trainer_image}" alt="">
+                                </div>
+                                <div class="trainerName">${item.fullname}</div>
+                                <div class="trainerPosition">${
+                                  item.profession
+                                }</div>
+                                <div class="trainerSocials">
+                                    <a href="${
+                                      item.facebook
+                                    }" target="_blank" class="trainer_social">
+                                        <i class="fa-brands fa-facebook"></i>
+                                    </a>
+                                    <a href="${
+                                      item.instagram
+                                    }" target="_blank" class="trainer_social">
+                                        <i class="fa-brands fa-instagram"></i>
+                                    </a>
+                                    <a href="${
+                                      item.twitter
+                                    }" target="_blank" class="trainer_social">
+                                        <i class="fa-brands fa-twitter"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <form action="" method="/" class="subscriptionInput">
+                                <input class="subscription__input" type="email" placeholder="Find a class"
+                                    required="required">
+                                <button class="subscriptionBtn">
+                                    <i class="fa-solid fa-arrow-right"></i>
+                                </button>
+                            </form>
+
+                            <div class="trainerModalWrap">
+                                <div class="trainerSlider">
+                                ${item.courses
+                                  .map(
+                                    (el) => `
+
+    
+                                 
+                                    
+                                        <div class="card">
+                                            <div class="trainerPreview">
+                                                <img src="${el.image}" alt="">
+                                                <p>${el.category}</p>
+                                            </div>
+                                            <div class="modalTrainerHead">
+                                                <div class="trainer__title">${el.description}</div>
+                                                <div class="trainerLevel">${el.level}</div>
+                                            </div>
+                                            <div class="trainerParameters">
+                                                <div class="trainerParameter">
+                                                   <i class="fa-brands fa-youtube"></i>
+                                                   <span>7</span>  
+                                                </div>
+                                                <div class="trainerParameter">
+                                                  <i class="fa-regular fa-user"></i> 
+                                                  <span>160</span> 
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                     `
+                                  )
+                                  .join("")}
+                                </div>
+                            </div>
+
+                            <div class="bottomBtns">
+                                <div class="arrowBtns">
+                                    <i class="fa-solid fa-left-long"></i>
+                                   <i class="fa-solid fa-right-long"></i>
+                                </div>
+                                <button class="btn btn_orange">Browse all class</button>
+                            </div>
+
+                            <button title="Close (Esc)" type="button" class="modalCloseBtn">
+                               <i class="fa-solid fa-xmark"></i>
+                            </button>
+
+                        </div>
+                    </div>
+            </div>
+     
+    `;
+  });
+  const trainers = [...document.querySelectorAll(".listWrap .trainerItem")];
+  const closeBtns = [...document.querySelectorAll(".modal .modalCloseBtn")];
+
+  trainers.forEach((trainer) => {
+    trainer.addEventListener("click", (e) => {
+      // trainer.nextElementSibling.style.display = "block";
+      trainer.nextElementSibling.classList.add("showed");
+      e.preventDefault();
+    });
+  });
+
+  closeBtns.forEach((closeBtn) => {
+    closeBtn.addEventListener("click", (e) => {
+      // closeBtn.closest(".modal").style.display = "none";
+      closeBtn.closest(".modal").classList.remove("showed");
+    });
+  });
 }
 
 async function fillDataToContainer(data, wrap) {
