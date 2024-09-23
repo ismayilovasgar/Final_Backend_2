2a68d736ef6de4d1359cf0dd8235c19e5534ac7f
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-09-23 23:30:13 +0400

    save

--- static/js/home/home.js
+++ static/js/home/home.js
@@ -199,11 +199,26 @@ var swiper = new Swiper(".swiper_content_steps", {
 
 // ? advantages list
 var swiper = new Swiper(".swiper_content_advantageslist", {
-  slidesPerView: "auto",
-  spaceBetween: 30,
+  slidesPerView: 3,
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 });
 
