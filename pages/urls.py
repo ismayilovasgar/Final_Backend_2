a8b1eb23b6caa7a0bf9c8bbed0b25c996b6e3573
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-09-27 16:13:43 +0400

    save

--- pages/urls.py
+++ pages/urls.py
@@ -25,9 +25,7 @@ urlpatterns = [
         "class_02/", views.class02__page, name="class02"
     ),  # http://127.0.0.1:8000/class_02
     path(
-        "class_02/category/<str:category_name>/",
-        views.class02_trainers,
-        name="class02_trainers",
+        "class_02/category/<str:category_name>/",views.class02_trainers,name="class02_trainers",
     ),  # http://127.0.0.1:8000/programs
     path(
         "singleblog/<int:id>/", views.blog_detail, name="blog_detail"
